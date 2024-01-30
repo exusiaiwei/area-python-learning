3527875f358d33762ba333294f167ee5bf692a07
wzc <exusiai.wei@outlook.com>
2024-01-30 09:14:17 +0800

    Update mymodule.py with docstrings

--- my_python_learning_zh/practical_code/modules/mymodule.py
+++ my_python_learning_zh/practical_code/modules/mymodule.py
@@ -1,5 +1,10 @@
-# 示例代码来源： A Byte of Python
+"""
+Example code source: A Byte of Python # This is a module-level docstring.
+"""
 def say_hi():
+    """
+    Print a greeting message to stdout. This is a function-level docstring.
+    """
     print('Hi, this is mymodule speaking.')
 
 __version__ = '0.1'
